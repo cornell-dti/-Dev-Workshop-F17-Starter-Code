6733d0790ebe1f71803cba3075ba5f44e20dc38c
Karun Singh <karun.singh17@gmail.com>
2017-09-24 08:11:00 -0400

    Added /api/clear_backend as an example

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const app = express();
+var request = require('request');
+
+// TODO: Change the constant below to be your NetID!
+const netid = "your-netid-here";
 
 app.get('/', function (req, res) {
+  // TODO(shea): render the HTML here
   res.send('Hello World!');
+});
+
+// TODO: Implement your API endpoints here! We've started you off with an example...
+
+// /api/clear_backend is used to reset your backend to its empty state.
+// This is what makes the 'Clear' button work - it simply deletes everything from the database,
+// which could be useful to you while testing.
+app.get('/api/clear_backend', function(req, res) {
+	request("https://cdti-db.herokuapp.com/"+netid+"/delete", function (error, response, body) {
+		if (error) {
+			res.send(error);
+			return;
+		}
+		res.send(body);
+	});
 });
 
 // This tells your server to start listening for requests on localhost:3000
